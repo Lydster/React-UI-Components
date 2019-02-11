dbe90295649aa1cc80cfc406279cc93dbfd7dc48
lydiathornton <lydia.d.thornton@gmail.com>
2019-02-11 14:56:22 -0800

    was stuck on file paths for a very long time

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,8 @@
 import React from 'react';
 import './Header.css';
+
+function HeaderTitle() {
+    return <h1><span>Lambda School </span>@LambdaSchool  26 jan</h1>;
+  }
+
+export default HeaderTitle;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import './Header.css';
+
+
+function ImageThumbnail() {
+   return ( <a href='https://photos.google.com/share/AF1QipMafcTqKR0MAWM2OD-iZ-iAHPUr3rcvvCUcWH8yE25rVbMdErofews6jxGRcqm_rA?key=WFdidE1sN1J1anBnZGNWdG5aeE9wYWVXUXZBY05n&source=ctrlq.org'>
+    <img src='https://lh3.googleusercontent.com/3UNGKULh1if6MwWL9ZNQ899csYSffZkrcCBmiteayQe3pzVCvudTEdcl-7_w1SpwE2dS8aHcLhEzJtvX-VFl8FSMzKH4CWHl6XiqiFi-jWPa_UWuXIC0GVqLeVHeJmvMs1D9BE4hVQ=s240-p-k' alt="lambda school logo"/></a>
+    )
+}
+
+export default ImageThumbnail;
