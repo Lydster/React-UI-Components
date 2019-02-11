c7c19b33bcb7f75eb85f26bbda74c620696dcf67
lydiathornton <lydia.d.thornton@gmail.com>
2019-02-11 15:12:55 -0800

    very close to MVP.

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -4,7 +4,7 @@ import './Card.css';
 function CardBanner() {
     return (
     <a href='https://photos.google.com/share/AF1QipNmwBC7I6DBvz0gSm4kSQmTMhgou7ABeRDG8unf9VeByJZw6E33jir2scg1jvfqRw?key=aExMVmFpWGdDX0FMdXozU1BhMXo4SXl1V2tobktB&source=ctrlq.org'>
-    <img src='https://lh3.googleusercontent.com/EUsr8FTftJVitFiAHUoIndmdO94ZrZLqCT0RbjDQJQJhFt1gzVgYDOHw5QPcnzWbXQoh67AmTu7laQmvzTo7fK9HIlmUQ0QBr__xwz6mC46KSIuvJu8XLLck22VtjxLX2-6Bq8jH2w=w2400' alt="react icon background" /></a> 
+    <img className="reactlogo" src='https://lh3.googleusercontent.com/EUsr8FTftJVitFiAHUoIndmdO94ZrZLqCT0RbjDQJQJhFt1gzVgYDOHw5QPcnzWbXQoh67AmTu7laQmvzTo7fK9HIlmUQ0QBr__xwz6mC46KSIuvJu8XLLck22VtjxLX2-6Bq8jH2w=w2400' alt="react icon background" /></a> 
     )
 }
 
