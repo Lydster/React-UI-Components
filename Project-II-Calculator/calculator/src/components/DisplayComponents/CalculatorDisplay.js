722c2b312eb8ff8207cc51b1dd7580cb5a91f457
lydiathornton <lydia.d.thornton@gmail.com>
2019-02-12 13:10:51 -0800

    started calc

--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,18 @@
 import React from 'react';
 import './Display.css';
+
+
+
+const DisplayObj = {
+    numbers: [0,1,2,3,4,5,6,7,8,9],
+    operators: ['+', '-', '*', '/', '=']
+}
+
+const CalculatorDisplay = props => {
+    return (
+       <div className='display'>0</div> 
+    );
+}
+
+
+export default CalculatorDisplay;
